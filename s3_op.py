0238703f324baf56d209016afb8669b080ba1752
Nancy0401 <linsa0401@126.com>
2016-05-12 12:57:33 +0800

    modify access amazon s3

--- s3_op.py
+++ s3_op.py
@@ -14,11 +14,8 @@ from boto3 import Session
 
 
 	
-def check_bucket(access_key , secret_key,region , bucket_name):
-	session = Session(aws_access_key_id=access_key,
-		aws_secret_access_key=secret_key,
-		region_name=region)
-	s3 = session.resource('s3')
+def check_bucket( region , bucket_name):
+	s3 = boto3.resource('s3')
 
 	bucket = s3.Bucket(bucket_name)
 	try:
@@ -32,10 +29,10 @@ def check_bucket(access_key , secret_key,region , bucket_name):
 	print '\n Check bucket : %s  successful !! '%bucket_name
 	return s3
 
-def upload_res(access_key, secret_key, region, bucket_name, files):
+def upload_res( region, bucket_name, files):
 
 	try:
-		s3 = check_bucket(access_key , secret_key, region , bucket_name)
+		s3 = check_bucket(region , bucket_name)
 		i = 1
 		for file_name in files:		
 			print '\n Waiting upload : %s --> %s'%(file_name ,bucket_name)
@@ -44,7 +41,7 @@ def upload_res(access_key, secret_key, region, bucket_name, files):
 				print ' Upload Done  : %s --> %s \n'%(file_name , bucket_name)
 			
 	except Exception as e :
-		print 'upload error e ',e 
+		print '[Error] Amazon S3 :  ',e 
 
 
 def menu():
@@ -57,10 +54,7 @@ def menu():
 					default='mk-community', help='uplload bucket : ')  
 	parser.add_option('-r','--region' , dest = 'region' ,
 				default='eu-west-1', help = 'Region ')
-	parser.add_option('-a', '--access_key', dest='access_key',
-					  default='', help='AWS account access_key'),  
-        parser.add_option('-s', '--secret_key', dest='secret_key',
-					  default='', help='AWS account secret_key'),  
+
 	options , args = parser.parse_args()
 	return options ,args
 
@@ -73,7 +67,7 @@ if __name__ == '__main__':
 			sys.exit(550)
 		files = res_content.split('\n')
                 print "Files : ",files
-		upload_res(options.access_key, options.secret_key, options.region, options.bucket_name, files )
+		upload_res(options.region, options.bucket_name, files )
 	else:
 		print '\n [Error] : pls check parameter \n' 
 
